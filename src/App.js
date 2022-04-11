2b92120c70d9998cf73383a14f223ec602b2143c
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-04-11 03:15:22 -0300

    ADD basename

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
+import { Router } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
   return (
-    <Switch>
-      <Route component={ Login } path="/" exact />
-      <Route component={ Wallet } path="/carteira" />
-    </Switch>
+    <Router basename="https://pabloalmeidac.github.io/trybewallet.github.io/">
+      <Switch>
+        <Route component={ Login } path="/" exact />
+        <Route component={ Wallet } path="/carteira" />
+      </Switch>
+    </Router>
   );
 }
 
