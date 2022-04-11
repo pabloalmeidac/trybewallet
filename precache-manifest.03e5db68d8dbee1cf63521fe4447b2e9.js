d55dc7dbda1b8513d5132e9e2eb4e6cf817f6bf3
pabloalmeidac <pabloalmeidac@users.noreply.github.com>
2022-04-11 05:36:34 +0000

    deploy: 2e7cb9100bb97e10ca7350132740c51b6c216dcb

---
+++ precache-manifest.03e5db68d8dbee1cf63521fe4447b2e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9397864b82cbf1c496244d4791099adb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f3e372224a42fff0b0d",
+    "url": "/static/css/main.6cc7b6dc.chunk.css"
+  },
+  {
+    "revision": "ec76ed20528a0dec7c30",
+    "url": "/static/js/2.2a56b7cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2a56b7cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f3e372224a42fff0b0d",
+    "url": "/static/js/main.f35395b6.chunk.js"
+  },
+  {
+    "revision": "fe617c2cb41aece9e721",
+    "url": "/static/js/runtime-main.e8df815d.js"
+  }
+]);
