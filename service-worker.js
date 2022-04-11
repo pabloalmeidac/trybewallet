3ad64c35678f8015bc1ffcdb08143d902f2930cf
pabloalmeidac <pabloalmeidac@users.noreply.github.com>
2022-04-11 06:58:02 +0000

    deploy: 3dde9a5956454ac7b9133b20c8a4ba027b629733

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trybewallet/precache-manifest.5625d7895ab308a001a94665a25ad8aa.js"
+  "/precache-manifest.476f1f97a950657d6f8aa0baf9a30176.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trybewallet/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
