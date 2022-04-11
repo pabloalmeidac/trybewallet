7e63603d4eb6f9eb5cb4c22997add052fd8bfbcd
pabloalmeidac <pabloalmeidac@users.noreply.github.com>
2022-04-11 06:16:20 +0000

    deploy: 2b92120c70d9998cf73383a14f223ec602b2143c

---
+++ precache-manifest.260261aa16e85c29f727adb05d045c58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7a81e53aeb79af24b17a835bac868a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df35f461188b8fa70d34",
+    "url": "/static/css/main.6cc7b6dc.chunk.css"
+  },
+  {
+    "revision": "2a9b4500bd74b21b3dd1",
+    "url": "/static/js/2.387282a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.387282a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df35f461188b8fa70d34",
+    "url": "/static/js/main.2cebd8a0.chunk.js"
+  },
+  {
+    "revision": "f7b511fb6f03f4435ca8",
+    "url": "/static/js/runtime-main.01f50e13.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trybewallet.github.io/precache-manifest.103d07c6233846ca7a49835591b1cfe0.js"
+  "/precache-manifest.260261aa16e85c29f727adb05d045c58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trybewallet.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
