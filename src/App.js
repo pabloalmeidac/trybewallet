bf75ffe47eb0df1f8e2acd36aa4b756da7e9267a
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-04-11 03:23:42 -0300

    Refactor project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import { Router } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
   return (
-    <Router basename="https://pabloalmeidac.github.io/trybewallet.github.io/">
-      <Switch>
-        <Route component={ Login } path="/" exact />
-        <Route component={ Wallet } path="/carteira" />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/wallet" component={ Wallet } />
+    </Switch>
   );
 }
 
